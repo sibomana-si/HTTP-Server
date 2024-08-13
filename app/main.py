6361230ca0006754197d375bd4da376e8db9c00b
simon <si.sibomana@gmail.com>
2024-08-13 07:18:10 +0200

    respond with 200

--- app/main.py
+++ app/main.py
@@ -1,14 +1,26 @@
-# Uncomment this to pass the first stage
+
 import socket
 
+
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    print("Sever running on port 4221....")
+
+    try:
+        client_socket, client_address = server_socket.accept()
+        print(f"Connection accepted from {client_address}.")
+
+        client_data = client_socket.recv(1024)
+        print(f"client data: {client_data.decode()}")
+
+        response = "HTTP/1.1 200 OK\r\n\r\n"
+        client_socket.sendall(response.encode())
+        client_socket.close()
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        server_socket.close()
+        print("Server shut down")
 
 
 if __name__ == "__main__":
