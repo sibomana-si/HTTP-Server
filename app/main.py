3d62fbc5c53072f9237c0a803f2a94bc94015d82
simon <si.sibomana@gmail.com>
2024-08-20 18:42:26 +0200

    read request body

--- app/main.py
+++ app/main.py
@@ -20,27 +20,18 @@ async def client_handler(reader, writer):
 async def generate_response(request):
     client_request = request.decode().split('\r\n')
     try:
-        request_line = client_request[0]
-        request_target = request_line.split()[1]
+        request_target = client_request[0].split()[1]
         base_url = request_target.split("/")[1]
-        request_headers = {}
         response = "HTTP/1.1 404 Not Found\r\n\r\n"
-
-        for line in client_request[1:]:
-            if line:
-                header = line.split(":")[0]
-                if header in {"Host", "User-Agent", "Accept"}:
-                    request_headers[header] = line
-        user_agent_header = request_headers.get("User-Agent", "")
 
         if base_url == "":
             response = "HTTP/1.1 200 OK\r\n\r\n"
         elif base_url == "echo":
-            response = await get_echo_response(request_target)
+            response = await get_echo_response(client_request)
         elif base_url == "files":
-            response = await get_files_response(request_target)
+            response = await get_files_response(client_request)
         elif base_url == "user-agent":
-            response = await get_user_agent_response(user_agent_header)
+            response = await get_user_agent_response(client_request)
 
         return response
     except Exception as ex:
@@ -48,7 +39,8 @@ async def generate_response(request):
         raise e
 
 
-async def get_echo_response(request_target):
+async def get_echo_response(client_request):
+    request_target = client_request[0].split()[1]
     content_type = "text/plain"
     response_status_line = "HTTP/1.1 200 OK\r\n"
     response_body = request_target.split("/")[2]
@@ -58,21 +50,36 @@ async def get_echo_response(request_target):
     return response
 
 
-async def get_files_response(request_target):
+async def get_files_response(client_request):
+    request_data = client_request[-1]
+    http_method, request_target, http_version = client_request[0].split()
     target_file = Path(file_dir + request_target.split("/")[2])
-    if target_file.is_file():
-        content_type = "application/octet-stream"
-        response_status_line = "HTTP/1.1 200 OK\r\n"
-        content_length = target_file.stat().st_size
-        response_body = target_file.read_text()
-        response_headers = f"Content-Type: {content_type}\r\nContent-Length: {content_length}\r\n\r\n"
-        response = f"{response_status_line}{response_headers}{response_body}"
-    else:
-        response = "HTTP/1.1 404 Not Found\r\n\r\n"
+    response = "HTTP/1.1 404 Not Found\r\n\r\n"
+
+    if http_method == "GET":
+        if target_file.is_file():
+            content_type = "application/octet-stream"
+            response_status_line = "HTTP/1.1 200 OK\r\n"
+            content_length = target_file.stat().st_size
+            response_body = target_file.read_text()
+            response_headers = f"Content-Type: {content_type}\r\nContent-Length: {content_length}\r\n\r\n"
+            response = f"{response_status_line}{response_headers}{response_body}"
+    elif http_method == "POST":
+        target_file.write_text(request_data)
+        response = "HTTP/1.1 201 Created\r\n\r\n"
+
     return response
 
 
-async def get_user_agent_response(user_agent_header):
+async def get_user_agent_response(client_request):
+    request_headers = {}
+    for line in client_request[1:]:
+        if line:
+            header = line.split(":")[0]
+            if header in {"Host", "User-Agent", "Accept"}:
+                request_headers[header] = line
+    user_agent_header = request_headers.get("User-Agent", "")
+
     content_type = "text/plain"
     response_status_line = "HTTP/1.1 200 OK\r\n"
     response_body = user_agent_header.split(":")[1].strip()
