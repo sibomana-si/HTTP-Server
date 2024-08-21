76348b3afc01a58b7cd49dd1ed7a93c97b0dabba
simon <si.sibomana@gmail.com>
2024-08-21 16:49:03 +0200

    compression headers

--- app/main.py
+++ app/main.py
@@ -13,8 +13,8 @@ async def client_handler(reader, writer):
         writer.write(response.encode())
         await writer.drain()
         writer.close()
-    except Exception as e:
-        print(f"ERROR in client_handler: {client_address}|{e}")
+    except Exception as ex:
+        print(f"ERROR in client_handler: {client_address}|{ex}")
 
 
 async def generate_response(request):
@@ -23,29 +23,42 @@ async def generate_response(request):
         request_target = client_request[0].split()[1]
         base_url = request_target.split("/")[1]
         response = "HTTP/1.1 404 Not Found\r\n\r\n"
+        request_headers = {}
+        for line in client_request[1:]:
+            if line:
+                header = line.split(":")[0]
+                if header in {"Host", "User-Agent", "Accept", "Accept-Encoding"}:
+                    request_headers[header] = line
+        user_agent_header = request_headers.get("User-Agent", "")
+        accept_encoding_header = request_headers.get("Accept-Encoding", "")
 
         if base_url == "":
             response = "HTTP/1.1 200 OK\r\n\r\n"
         elif base_url == "echo":
-            response = await get_echo_response(client_request)
+            response = await get_echo_response(client_request, accept_encoding_header)
         elif base_url == "files":
             response = await get_files_response(client_request)
         elif base_url == "user-agent":
-            response = await get_user_agent_response(client_request)
+            response = await get_user_agent_response(user_agent_header)
 
         return response
     except Exception as ex:
         print(f"ERROR in generate_response: {client_request}|{ex}")
-        raise e
+        raise ex
 
 
-async def get_echo_response(client_request):
+async def get_echo_response(client_request, accept_encoding_header):
     request_target = client_request[0].split()[1]
     content_type = "text/plain"
     response_status_line = "HTTP/1.1 200 OK\r\n"
+    request_compression = accept_encoding_header.split(":")[1].strip()
     response_body = request_target.split("/")[2]
     content_length = len(response_body)
-    response_headers = f"Content-Type: {content_type}\r\nContent-Length: {content_length}\r\n\r\n"
+    if request_compression in compression_schemes:
+        response_headers = (f"Content-Type: {content_type}\r\nContent-Encoding: {request_compression}\r\n"
+                            f"Content-Length: {content_length}\r\n\r\n")
+    else:
+        response_headers = f"Content-Type: {content_type}\r\nContent-Length: {content_length}\r\n\r\n"
     response = f"{response_status_line}{response_headers}{response_body}"
     return response
 
@@ -71,15 +84,7 @@ async def get_files_response(client_request):
     return response
 
 
-async def get_user_agent_response(client_request):
-    request_headers = {}
-    for line in client_request[1:]:
-        if line:
-            header = line.split(":")[0]
-            if header in {"Host", "User-Agent", "Accept"}:
-                request_headers[header] = line
-    user_agent_header = request_headers.get("User-Agent", "")
-
+async def get_user_agent_response(user_agent_header):
     content_type = "text/plain"
     response_status_line = "HTTP/1.1 200 OK\r\n"
     response_body = user_agent_header.split(":")[1].strip()
@@ -106,6 +111,7 @@ if __name__ == "__main__":
         file_dir = ""
         if len(opts[0]) != 0:
             file_dir = opts[0][0][1]
+        compression_schemes = {"gzip", }
         asyncio.run(main())
     except (Exception, KeyboardInterrupt) as e:
         print(f"ERROR: {e}")
